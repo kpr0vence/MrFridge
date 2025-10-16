d27c1b7a53e4013bf74123cfdc241a4642dd82b8
Kati Provence <kproven1@unca.edu>
2025-10-15 22:59:10 -0400

    Added items accordian, input is flexible, but itemsDisplay parent content isn't

--- app/components/ItemCardHeader.tsx
+++ app/components/ItemCardHeader.tsx
@@ -4,8 +4,9 @@ import { item } from "../types";
 import { differenceInDays, isCloseToExpired, isExpired } from "./itemFunctions";
 
 
-interface ItemCardProps {
+interface ItemCardHeaderProps {
     item: item
+    isActive: any
 }
 
 function daysBubbleColor(item: item) {
@@ -16,19 +17,21 @@ function daysBubbleColor(item: item) {
     return "bg-[#41d78f] rounded-full justify-center items-center h-12 w-12"
 }
 
-export default function ItemCard({ item }: ItemCardProps) {
+export default function ItemCardHeader({ item, isActive }: ItemCardHeaderProps) {
     const daysLeft = differenceInDays(new Date(), item.expireDate)
+    const classNameClosed: string = "border-4 border-gray-200 rounded-lg w-full flex-row justify-between items-center p-2 gap-2 mb-4"
+    const classNameOpen: string = "border-4 border-b-2 border-gray-200 rounded-t-lg w-full flex-row justify-between items-center p-2 gap-2"
     return (
-        <View className="border-4 border-gray-200 rounded-lg w-full flex-row justify-between items-center p-2 gap-2" >
+        <View className={isActive ? classNameOpen : classNameClosed} >
             <Text className="text-black text-xl font-bold">{item.name}</Text>
-            <View className="flex-row gap-10 bg-purple-600">
-                <View className="flex-row items-center bg-pink-500 gap-2">
+            <View className="flex-row gap-10">
+                <View className="flex-row items-center gap-2">
                     <View className={daysBubbleColor(item)}>
                         <Text className="text-white text-xl font-bold">{daysLeft}</Text>
                     </View>
                     <Text>{Math.abs(daysLeft) === 1? "Day": "Days"}</Text>
                 </View>
-                <Ionicons name={"chevron-down-outline"} color={"#9ca3af"} size={30} /> 
+                <Ionicons name={isActive ? "chevron-up-outline" : "chevron-down-outline"} color={"#9ca3af"} size={30} /> 
             </View>
             
 
---
+++ app/components/ItemsAccordian.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import Accordion from 'react-native-collapsible/Accordion';
+import { fridge } from "../dummyData";
+import { item } from '../types';
+import ItemCardContent from './ItemCardContent';
+import ItemCardHeader from "./ItemCardHeader";
+
+
+export default function ItemsAccordian() {
+  const [ activeSections, setActiveSections ] = useState([]);
+  const sections = fridge
+  
+
+  function renderHeader(section: item, _: any, isActive: any) {
+    return (
+      <ItemCardHeader item={section} isActive={isActive}/>
+    );
+  };
+
+  function renderContent(section: item, _: any, isActive: any) {
+    return (
+      <View className='border-4 border-t-0 border-gray-200  rounded-b-lg  w-full mb-4 p-4'>
+        < ItemCardContent item={section} />
+      </View>
+    );
+  }
+  return (
+          <Accordion
+            align="bottom"
+            sections={sections}
+            activeSections={activeSections}
+            renderHeader={renderHeader}
+            renderContent={renderContent}
+            underlayColor={"#f9fafb"}
+            onChange={(sections) => setActiveSections(sections)}
+            // sectionContainerStyle={styles.accordContainer}
+          />
+
+  );
+}
